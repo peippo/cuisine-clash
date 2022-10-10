ba9b60e175f6b7b57b7e4e6fc24766ecedfa064d
Janne Peippo <peippo@gmail.com>
2022-10-10 21:17:13 +0300

    Add Google Fonts

---
+++ src/pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&family=Source+Serif+Pro&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Source Sans Pro", "system-ui", "sans-serif"],
+      serif: ["Source Serif Pro", "ui-serif", "serif"],
+    },
   },
   plugins: [],
 };

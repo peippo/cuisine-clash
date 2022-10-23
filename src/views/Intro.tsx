bac68cdd8c5eafa9ce371d13aee0a9f2be0c0d61
Janne Peippo <peippo@gmail.com>
2022-10-23 18:10:39 +0300

    Adjust intro screen

--- src/views/Intro.tsx
+++ src/views/Intro.tsx
@@ -10,11 +10,24 @@ const Intro = () => {
       <Head>
         <title>Cuisine Clash</title>
       </Head>
-      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
-        <h1 className="text-4xl">Cuisine Clash</h1>
-        <Button onClickHandler={() => moveToView("DRAFTING")}>
-          Start Game
-        </Button>
+      <main className="mx-auto flex min-h-screen flex-col items-center justify-center bg-intro bg-cover bg-center bg-no-repeat p-4">
+        <img
+          src="logo.png"
+          srcSet="logo.png 1x, logo@2x.png 2x"
+          width="800"
+          height="369"
+          alt=""
+          className="animate-intro-logo"
+        />
+        <h1 className="sr-only">Cuisine Clash</h1>
+        <div className="flex animate-intro-content flex-col">
+          <p className="mb-5 text-2xl text-indigo-400">
+            A food fighting card game
+          </p>
+          <Button onClickHandler={() => moveToView("DRAFTING")}>
+            Start Game
+          </Button>
+        </div>
       </main>
     </>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,40 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        intro: "url('/intro-bg.svg')",
+      },
+      keyframes: {
+        zoomInDown: {
+          "0%": {
+            "animation-timing-function": "cubic-bezier(.55,.055,.675,.19)",
+            opacity: 0,
+            transform: "scale3d(.1,.1,.1) translate3d(0,-500px,0)",
+          },
+          "60%": {
+            "animation-timing-function": "cubic-bezier(.175,.885,.32,1)",
+            opacity: 1,
+            transform: "scale3d(.475,.475,.475) translate3d(0,60px,0)",
+          },
+        },
+        delayFadeIn: {
+          "0%": {
+            opacity: 0,
+          },
+          "50%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        "intro-logo": "zoomInDown 1.5s linear",
+        "intro-content": "delayFadeIn 3s ease-in",
+      },
+    },
     fontFamily: {
       sans: ["Source Sans Pro", "system-ui", "sans-serif"],
       serif: ["Source Serif Pro", "ui-serif", "serif"],

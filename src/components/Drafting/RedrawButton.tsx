129996a04c0d4f3af229cc9bbb7f6a6e2cd008b7
Janne Peippo <peippo@gmail.com>
2022-10-23 18:10:55 +0300

    Adjust button styling

--- src/components/Drafting/RedrawButton.tsx
+++ src/components/Drafting/RedrawButton.tsx
@@ -15,7 +15,7 @@ const RedrawButton = () => {
   return (
     <button
       className={classNames(
-        "group relative self-center rounded-xl border-t-2 border-l-2 border-indigo-600 bg-gradient-to-b from-indigo-700 to-indigo-900 font-serif text-cyan-100",
+        "group relative self-center rounded-lg border-t-2 border-l-2 border-indigo-600 bg-gradient-to-b from-indigo-700 to-indigo-900 text-cyan-100",
         "disabled:border-slate-600 disabled:from-slate-700 disabled:to-slate-700 disabled:text-slate-400",
         "hover:border-indigo-700 hover:bg-gradient-to-b hover:from-indigo-900 hover:to-indigo-800"
       )}
@@ -24,14 +24,14 @@ const RedrawButton = () => {
     >
       <span
         className={classNames(
-          "absolute -right-2 -top-2 flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-b from-purple-500 via-purple-800 to-fuchsia-900 text-3xl text-white drop-shadow-md",
+          "absolute -right-2 -top-3 flex h-16 w-16 items-center justify-center rounded-full bg-gradient-to-b from-purple-500 via-purple-800 to-fuchsia-900 text-3xl text-white drop-shadow-md",
           "group-disabled:from-stone-500 group-disabled:via-stone-800 group-disabled:to-stone-900 group-disabled:text-stone-500"
         )}
       >
         {redrawsLeft}
       </span>
       <span className="sr-only">redraws available</span>
-      <span className="flex py-2 pr-16 pl-5 text-lg">
+      <span className="flex pt-1 pb-2 pr-20 pl-5 text-lg">
         {hasRedraws
           ? `Redraw ${MAX_DRAFT_CARDS - playerCards.length} cards`
           : "No redraws left"}

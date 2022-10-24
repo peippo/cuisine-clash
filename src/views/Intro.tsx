91b21b719bf9b656c38e49fb0733c94c4f249e41
Janne Peippo <peippo@gmail.com>
2022-10-24 18:26:03 +0300

    Use next/image component

--- src/views/Intro.tsx
+++ src/views/Intro.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { useStore } from "@store/store";
+
+import Image from "next/image";
 import Button from "@components/Button";
 
 const Intro = () => {
@@ -11,13 +13,12 @@ const Intro = () => {
         <title>Cuisine Clash</title>
       </Head>
       <main className="mx-auto flex min-h-screen flex-col items-center justify-center bg-intro bg-cover bg-center bg-no-repeat p-4">
-        <img
-          src="logo.png"
-          srcSet="logo.png 1x, logo@2x.png 2x"
+        <Image
+          src="/logo.png"
+          className="motion-safe:animate-intro-logo"
           width="800"
           height="369"
           alt=""
-          className="motion-safe:animate-intro-logo"
         />
         <h1 className="sr-only">Cuisine Clash</h1>
         <div className="flex flex-col motion-safe:animate-intro-content">

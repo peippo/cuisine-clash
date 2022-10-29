1dba0d10d21917e077fc3e636827de084a93b4c2
Janne Peippo <peippo@gmail.com>
2022-10-29 17:21:24 +0300

    Improve turn speed selector accessibility

--- src/components/Battle/TurnSpeedSelector.tsx
+++ src/components/Battle/TurnSpeedSelector.tsx
@@ -9,7 +9,7 @@ const TurnSpeedSelector = () => {
   const turnSpeed = useStore((state) => state.turnSpeed);
   const setTurnSpeed = useStore((state) => state.setTurnSpeed);
 
-  const containerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLFieldSetElement>(null);
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
@@ -22,49 +22,59 @@ const TurnSpeedSelector = () => {
     return () => ctx.revert();
   }, []);
 
-  // FIXME: Use radio buttons
-
   return (
-    <div
+    <fieldset
       ref={containerRef}
       className="absolute top-2 right-2 z-10 flex rounded-md"
     >
-      <span className="sr-only">Set turn message speed</span>
+      <legend className="sr-only">Set turn message speed</legend>
 
-      <button
+      <label
         className={classNames(
           "flex h-7 w-7 items-center justify-center rounded-l-md bg-slate-700 p-2 text-slate-400 hover:cursor-pointer",
           turnSpeed === 600 && "bg-indigo-600 text-slate-200"
         )}
-        onClick={() => setTurnSpeed(600)}
-        aria-selected={turnSpeed === 600}
       >
+        <input
+          type="radio"
+          onChange={() => setTurnSpeed(600)}
+          checked={turnSpeed === 600}
+          className="sr-only"
+        />
         <PlayIcon width="10" />
         <span className="sr-only">Normal</span>
-      </button>
-      <button
+      </label>
+      <label
         className={classNames(
           "flex h-7 w-7 items-center justify-center bg-slate-700 p-2 text-slate-400 hover:cursor-pointer",
           turnSpeed === 300 && "bg-indigo-600 text-slate-200"
         )}
-        onClick={() => setTurnSpeed(300)}
-        aria-selected={turnSpeed === 300}
       >
+        <input
+          type="radio"
+          onChange={() => setTurnSpeed(300)}
+          checked={turnSpeed === 300}
+          className="sr-only"
+        />
         <ForwardIcon width="12" />
         <span className="sr-only">Fast</span>
-      </button>
-      <button
+      </label>
+      <label
         className={classNames(
           "flex h-7 w-7 items-center justify-center rounded-r-md bg-slate-700 p-2 text-slate-400 hover:cursor-pointer",
           turnSpeed === 150 && "bg-indigo-600 text-slate-200"
         )}
-        onClick={() => setTurnSpeed(150)}
-        aria-selected={turnSpeed === 150}
       >
+        <input
+          type="radio"
+          onChange={() => setTurnSpeed(150)}
+          checked={turnSpeed === 150}
+          className="sr-only"
+        />
         <FastForwardIcon width="12" />
         <span className="sr-only">Fastest</span>
-      </button>
-    </div>
+      </label>
+    </fieldset>
   );
 };
 

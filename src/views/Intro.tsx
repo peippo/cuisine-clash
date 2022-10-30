7826cecc9fe1d872685737a0a75d82fd67e301b2
Janne Peippo <peippo@gmail.com>
2022-10-30 15:21:03 +0200

    Add info links to intro screen

--- src/views/Intro.tsx
+++ src/views/Intro.tsx
@@ -18,26 +18,47 @@ const Intro: React.FC<Props> = ({ isSmallScreen }) => {
         <title>Cuisine Clash</title>
       </Head>
       <main className="mx-auto flex min-h-screen flex-col items-center justify-center bg-intro bg-cover bg-center bg-no-repeat p-4">
-        <Image
-          src="/logo.png"
-          className="motion-safe:animate-intro-logo"
-          width="800"
-          height="369"
-          alt=""
-        />
+        <div className="mt-auto">
+          <Image
+            src="/logo.png"
+            className="motion-safe:animate-intro-logo"
+            width="800"
+            height="369"
+            alt=""
+          />
+        </div>
         <h1 className="sr-only">Cuisine Clash</h1>
-        <div className="flex flex-col items-center motion-safe:animate-intro-content">
-          <p className="mb-5 text-lg text-indigo-400 md:text-2xl lg:text-3xl">
+        <div className="mb-auto flex flex-col items-center motion-safe:animate-intro-content">
+          <p className="mb-5 text-xl text-indigo-400 md:text-2xl lg:text-3xl">
             A food fighting card game
           </p>
           {isSmallScreen && <ScreenSizeMessage />}
           <Button
             onClickHandler={() => moveToView("DRAFTING")}
             isDisabled={isSmallScreen}
-            className="mt-6"
+            className="mt-6 mb-16"
           >
             Start Game
           </Button>
+        </div>
+
+        <div className="mb-6 mt-6 flex text-sm motion-safe:animate-intro-content">
+          <div className="mr-5 border-r border-slate-500 pr-5">
+            <a
+              className="border-b border-transparent transition-colors hover:border-slate-500 hover:text-slate-300"
+              href="https://fineli.fi/fineli/en/ohje/19?"
+            >
+              Food nutrition data from Fineli
+            </a>
+          </div>
+          <div>
+            <a
+              className=" border-b border-transparent transition-colors hover:border-slate-500 hover:text-slate-300"
+              href="https://github.com/peippo/cuisine-clash"
+            >
+              Github
+            </a>
+          </div>
         </div>
       </main>
     </>

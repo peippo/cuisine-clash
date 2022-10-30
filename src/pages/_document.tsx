d1603f10b4b037b660405f02cda37bba510fe467
Janne Peippo <peippo@gmail.com>
2022-10-30 17:44:41 +0200

    Add favicon & share image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="description" content="A food fighting card game" />
+        <meta property="og:title" content="Cuisine Clash" />
+        <meta property="og:description" content="A food fighting card game" />
+        <meta
+          property="og:image"
+          content="https://cuisine-clash.vercel.app/share.png"
+        />
+        <meta
+          property="twitter:image"
+          content="https://cuisine-clash.vercel.app/share.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
